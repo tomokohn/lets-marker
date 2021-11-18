bf9964830cb1ed8dec79332de255b94a04099107
tomokohn <tomokohn@gmail.com>
2021-11-19 01:33:53 +0200

    Added svg as object

--- src/index.js
+++ src/index.js
@@ -9,3 +9,15 @@ import { externalModule } from './js/myModule';
 import './styles/main.scss';
 
 externalModule.greet();
+
+// my code to refactor: 
+
+import Notebook from './images/header-svg/notebook.svg'
+
+
+const loadSvgNote = () => {
+    const SVG = `<object  data="${Notebook}" type="image/svg+xml"></object>`
+    document.querySelector('.container-svg').innerHTML = (SVG);
+}
+
+document.addEventListener('DOMContentLoaded', loadSvgNote())
